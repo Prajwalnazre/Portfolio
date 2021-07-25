578caf98383d99c4da45525f6771a3c2fb59ad6a
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-25 13:34:24 +0530

    Created visitors model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class visitors(models.Model) :
+    name = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=250)
+    message = models.TextField(max_length=650)
