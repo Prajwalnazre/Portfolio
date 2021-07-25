408df5b6b2421e3615457d32fb6fa899a66a046b
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-26 00:14:51 +0530

    feature: Animation on navigation links

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,40 @@
 // index
-console.log("HEllo World");
+
+$(document).ready(function() {
+    $("#aboutMe").mouseenter(function(){
+        $(this).css("transition", "all 1s");
+        $(this).css("transform", "scale(1.1)")
+    }).mouseleave(function() {
+        $(this).css("transform", "scale(1)") 
+    })
+
+    $("#projects").mouseenter(function(){
+        $(this).css("transition", "all 1s");
+        $(this).css("transform", "scale(1.1)")
+    }).mouseleave(function() {
+        $(this).css("transform", "scale(1)") 
+    })
+
+    $("#technicalSkills").mouseenter(function(){
+        $(this).css("transition", "all 1s");
+        $(this).css("transform", "scale(1.1)")
+    }).mouseleave(function() {
+        $(this).css("transform", "scale(1)") 
+    })
+
+    $("#education").mouseenter(function(){
+        $(this).css("transition", "all 1s");
+        $(this).css("transform", "scale(1.1)")
+    }).mouseleave(function() {
+        $(this).css("transform", "scale(1)") 
+    })
+
+    $("#contactMe").mouseenter(function(){
+        $(this).css("transition", "all 1s");
+        $(this).css("transform", "scale(1.1)")
+    }).mouseleave(function() {
+        $(this).css("transform", "scale(1)") 
+    })
+
+
+});
