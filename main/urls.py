7f92980865da7946e99fdf69392959c3b7d1382c
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-25 12:46:22 +0530

    Added and configured static files

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
