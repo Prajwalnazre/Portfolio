eb89f415b3aac9e543afacca54bc58969172229c
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-26 00:23:15 +0530

    feature: border animation on navigation links

--- static/js/index.js
+++ static/js/index.js
@@ -4,37 +4,56 @@ $(document).ready(function() {
     $("#aboutMe").mouseenter(function(){
         $(this).css("transition", "all 1s");
         $(this).css("transform", "scale(1.1)")
+        $(this).css("border-top", "4px solid white");
+        $(this).css("border-bottom", "4px solid white");
     }).mouseleave(function() {
-        $(this).css("transform", "scale(1)") 
+        $(this).css("transform", "scale(1)");
+        $(this).css("border-top", "none");
+        $(this).css("border-bottom", "none"); 
     })
 
     $("#projects").mouseenter(function(){
         $(this).css("transition", "all 1s");
         $(this).css("transform", "scale(1.1)")
+        $(this).css("border-top", "4px solid white");
+        $(this).css("border-bottom", "4px solid white");
     }).mouseleave(function() {
-        $(this).css("transform", "scale(1)") 
+        $(this).css("transform", "scale(1)");
+        $(this).css("border-top", "none");
+        $(this).css("border-bottom", "none");  
     })
 
     $("#technicalSkills").mouseenter(function(){
         $(this).css("transition", "all 1s");
-        $(this).css("transform", "scale(1.1)")
+        $(this).css("transform", "scale(1.1)");
+        $(this).css("border-top", "4px solid white");
+        $(this).css("border-bottom", "4px solid white");
     }).mouseleave(function() {
-        $(this).css("transform", "scale(1)") 
+        $(this).css("transform", "scale(1)");
+        $(this).css("border-top", "none");
+        $(this).css("border-bottom", "none");  
     })
 
     $("#education").mouseenter(function(){
         $(this).css("transition", "all 1s");
-        $(this).css("transform", "scale(1.1)")
+        $(this).css("transform", "scale(1.1)");
+        $(this).css("border-top", "4px solid white");
+        $(this).css("border-bottom", "4px solid white");
     }).mouseleave(function() {
-        $(this).css("transform", "scale(1)") 
+        $(this).css("transform", "scale(1)");
+        $(this).css("border-top", "none");
+        $(this).css("border-bottom", "none");  
     })
 
     $("#contactMe").mouseenter(function(){
         $(this).css("transition", "all 1s");
-        $(this).css("transform", "scale(1.1)")
+        $(this).css("transform", "scale(1.1)");
+        $(this).css("border-top", "4px solid white");
+        $(this).css("border-bottom", "4px solid white");
     }).mouseleave(function() {
-        $(this).css("transform", "scale(1)") 
+        $(this).css("transform", "scale(1)");
+        $(this).css("border-top", "none");
+        $(this).css("border-bottom", "none");  
     })
-
 
 });
