7779ba05e77a5b5d0aaded47df57b6960f7c063b
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-25 19:44:58 +0530

    test: Hello World testing on all static files

--- main/views.py
+++ main/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+def mainpage(request) :
+    return render(request, '../templates/index.html')
+
