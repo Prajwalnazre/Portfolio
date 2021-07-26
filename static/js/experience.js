c27d0cb7ec3da24312ec2f3dd6e23e431acaf0cd
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-27 01:40:39 +0530

    feature: Experience section completed

--- static/js/experience.js
+++ static/js/experience.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    
+    $("#expNext").on("click", function(){
+        $(this).css("color","lightgrey");
+        $("#expPrev").css("color","white");
+        $("#expObj").css("display","none");
+        $("#expApp").css("display", "none");
+        $("#expTake").css("display", "block");
+        $("#expRes").css("display", "block");
+    });
+
+    $("#expPrev").on("click", function(){
+        $(this).css("color","lightgrey");
+        $("#expNext").css("color","white");
+        $("#expObj").css("display","block");
+        $("#expApp").css("display", "block");
+        $("#expTake").css("display", "none");
+        $("#expRes").css("display", "none");
+    });
 });
