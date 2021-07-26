1d673f3b0ddf6413eaf6d42a809ba19fe1106c34
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-26 14:28:25 +0530

    feature: #7 done perfectly tested

--- static/js/index.js
+++ static/js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         $(this).css("border-bottom", "none");  
     })
 
-    $("#technicalSkills").mouseenter(function(){
+    $("#experience").mouseenter(function(){
         $(this).css("transition", "all 1s");
         $(this).css("transform", "scale(1.1)");
         $(this).css("border-top", "4px solid white");
@@ -93,10 +93,9 @@ $(document).ready(function() {
 
         if(index == 0)
         {
-
             // Section above - contactContainer
             // Section below - projectsContainer
-            // Curretn Section - aboutMeContainer
+            // Current Section - aboutMeContainer
 
             $("#contactContainer").addClass("animate__backOutUp");
             $("#contactContainer").removeClass("animate__fadeInDown");
@@ -113,7 +112,11 @@ $(document).ready(function() {
         
         if(index == 1)
         {
-            // console.log("Inside");
+
+            // Section above - aboutMeContainer
+            // Section below - experienceContainer
+            // Current Section - projectsContainer
+
             $("#aboutMeContainer").addClass("animate__backOutUp");
             $("#aboutMeContainer").removeClass("animate__fadeInDown");
             $("#experienceContainer").addClass("animate__backOutUp");
@@ -127,7 +130,62 @@ $(document).ready(function() {
             },400);
         }
 
+        if(index == 2)
+        {
+            // Section above - projectsContainer
+            // Section below - educationContainer
+            // Current Section - experienceContainer
 
+            $("#projectsContainer").addClass("animate__backOutUp");
+            $("#projectsContainer").removeClass("animate__fadeInDown");
+            $("#educationContainer").addClass("animate__backOutUp");
+            $("#educationContainer").removeClass("animate__fadeInDown");
+            
+            setTimeout(function(){
+                $("#projectsContainer").css("display", "none");
+                $("#educationContainer").css("display", "none");
+                $("#experienceContainer").css("display", "block");
+                $("#experienceContainer").addClass("animate__fadeInDown");
+            },400);
+        }
+
+        if(index == 3)
+        {
+            // Section above - experienceContainer
+            // Section below - contactContainer
+            // Current Section - educationContainer
+
+            $("#experienceContainer").addClass("animate__backOutUp");
+            $("#experienceContainer").removeClass("animate__fadeInDown");
+            $("#contactContainer").addClass("animate__backOutUp");
+            $("#contactContainer").removeClass("animate__fadeInDown");
+            
+            setTimeout(function(){
+                $("#experienceContainer").css("display", "none");
+                $("#contactContainer").css("display", "none");
+                $("#educationContainer").css("display", "block");
+                $("#educationContainer").addClass("animate__fadeInDown");
+            },400);
+        }
+
+        if(index == 4)
+        {
+            // Section above - educationContainer
+            // Section below - aboutMeContainer
+            // Current Section - contactContainer
+
+            $("#educationContainer").addClass("animate__backOutUp");
+            $("#educationContainer").removeClass("animate__fadeInDown");
+            $("#aboutMeContainer").addClass("animate__backOutUp");
+            $("#aboutMeContainer").removeClass("animate__fadeInDown");
+            
+            setTimeout(function(){
+                $("#educationContainer").css("display", "none");
+                $("#aboutMeContainer").css("display", "none");
+                $("#contactContainer").css("display", "block");
+                $("#contactContainer").addClass("animate__fadeInDown");
+            },400);
+        }        
 
     });
 
